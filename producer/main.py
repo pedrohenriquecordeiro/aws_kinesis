9293b1693d0e3e547447c7fc2bc83384c342dc14
Pedro Henrique Cordeiro <pedro.henrique.cefetmg@gmail.com>
2024-10-21 22:48:20 -0300

    Update main.py

--- producer/main.py
+++ producer/main.py
@@ -66,5 +66,13 @@ if __name__ == "__main__":
     # Replace with your actual Firehose stream name
     firehose_stream_name = "PUT-S3-HwC6m"
 
-    # Send the data to the Firehose stream
-    send_data_to_firehose(firehose_stream_name)
+
+    while True:
+        try:
+            send_data_to_firehose(firehose_stream_name)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+
+        time_to_sleep = random.randint(1, 10)
+        logging.info(f"Sleeping for {time_to_sleep} seconds before the next iteration")
+        time.sleep(time_to_sleep)
